892dc9cd8ff69e3a6b2fb3fd4a5a4d1bc62d9b29
Paul Hebble <pjhebble@gmail.com>
2021-05-21 10:00:23 -0500

    Skip days with missing data

--- ckan_download_count_graph.py
+++ ckan_download_count_graph.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import os, sys, git, datetime, json, tempfile, subprocess, time
-from typing import Dict, List, Tuple, Any
+from typing import Dict, List, Tuple, Any, Set
 
 counts_file = "download_counts.json"
 ckan_meta   = os.environ['HOME'] + "/Downloads/KSP/CKAN-meta"
@@ -21,17 +21,18 @@ def json_from_git(hash: str) -> Dict[str, Any]:
 
 # Counts before 9/1/2018 don't include parent forks
 first_day   = datetime.date(2018, 9, 1)
-# SpaceDock was down this day or something
-missing_day = datetime.date(2019, 5, 11)
 
 identifiers = sys.argv[1:]
+started: Set[str] = set()
 with tempfile.NamedTemporaryFile(mode='w') as tmpf:
     print("DateTime", *identifiers, file=tmpf)
     for vals in git_hashes_and_timestamps(counts_file):
-        if vals[1].date() == missing_day or vals[1].date() < first_day:
+        if vals[1].date() < first_day:
             continue
         parsed = json_from_git(vals[0])
-        if any(ident in parsed for ident in identifiers):
+        found = {ident for ident in identifiers if ident in parsed}
+        if (found and not started) or (started and all(ident in found for ident in started)):
+            started |= found
             print(vals[1].isoformat(), end=' ', file=tmpf)
             for identifier in identifiers:
                 count = parsed.get(identifier, 0)
